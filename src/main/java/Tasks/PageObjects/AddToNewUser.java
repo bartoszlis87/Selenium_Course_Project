a08aa709aa2eaab346f91923a8d17a7328271111
Bartosz <bartoszlis87@gmail.com>
2021-11-02 22:37:13 +0100

    add WebDriverWait and cssSelector clickSignIn

--- src/main/java/Tasks/PageObjects/AddToNewUser.java
+++ src/main/java/Tasks/PageObjects/AddToNewUser.java
@@ -1,17 +1,25 @@
 package Tasks.PageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AddToNewUser {
 
     private static WebDriver driver;
+    private WebDriverWait wait;
+
+    By clickSignIn = By.cssSelector("#_desktop_user_info");
 
     public AddToNewUser(WebDriver driver) {
         this.driver = driver;
     }
 
-    public static AddToNewUser goTo(String codersLabAdressWebsite) {
+    public AddToNewUser goTo(String codersLabAdressWebsite) {
         driver.navigate().to(codersLabAdressWebsite);
+        wait = new WebDriverWait(driver, 7);
+        wait.until(ExpectedConditions.elementToBeClickable(clickSignIn));
         return new AddToNewUser(driver);
     }
 
